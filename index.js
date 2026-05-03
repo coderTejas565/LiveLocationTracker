971255710b2fc1557334b1a94764a85752bc02e2
Tejas Avhad <codingwithtejas7@gmail.com>
2026-05-03 13:00:49 +0530

    feat: integrate Socket.io for real-time location updates from client

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import http from "node:http"
 import path from "node:path";
 
 import express from "express"
-import { Server } from "socket.io"
+import { Server, Socket } from "socket.io"
 
 async function main() {
     const PORT = process.env.PORT ?? 8000;
@@ -12,6 +12,16 @@ async function main() {
     const io = new Server()
 
     io.attach(server)
+
+    io.on('connection', (socket) => {
+        console.log(`[Socket:${socket.id}]: Connected Sucessfully`);
+        
+        socket.on('client:location:update', (locationData) => {
+            const { latitude, longitude} = locationData
+            console.log(`[Socket:${socket.id}]:client:location:update:`, locationData);
+            
+        })
+    })
 
     app.use(express.static(path.resolve('./public')))
 
